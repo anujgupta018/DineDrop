c680c9cfc0cd78f26b5c87915dc7ecd9b07fb387
Anuj <invincibleanuj1718@gmail.com>
2024-08-08 16:02:43 +0530

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Success from "./Pages/Success";
+import Error from "./Pages/Error";
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
