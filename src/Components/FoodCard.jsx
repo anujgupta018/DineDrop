4075afa114ef05a0d0597de9c81ed5cf149a583c
Anuj <invincibleanuj1718@gmail.com>
2024-08-08 22:47:07 +0530

    Added Components Section

---
+++ src/Components/FoodCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const FoodCard = () => {
+  return (
+    <div>
+      <img
+        src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5"
+        alt=""
+      />
+      <div>
+        <h2>Onion Pizza</h2>
+        <span>180</span>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-col lg:flex-row py-3 justify-between mx-6 mb-10">
+      <div>
+        <h3 className="text-xl font-bold text-gray-600">
+          {new Date().toUTCString().slice(0, 16)}
+        </h3>
+        <h1 className="text-2xl font-bold">DineDrop</h1>
+      </div>
+      <div>
+        <input
+          type="search"
+          name="Search"
+          id=""
+          placeholder="Search Here"
+          autoComplete="off"
+          className="p-3 border border-gray-400 text-sm rounded-lg outline-none w-full lg:w-[25vw]"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
