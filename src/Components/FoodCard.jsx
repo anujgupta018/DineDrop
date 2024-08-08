9762e42b274319991968496dbe1f7b7137edb6ab
Anuj <invincibleanuj1718@gmail.com>
2024-08-08 23:34:49 +0530

    Added FoodCart Section

--- src/Components/FoodCard.jsx
+++ src/Components/FoodCard.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-
-const FoodCard = () => {
+import { AiFillStar } from "react-icons/ai";
+const FoodCard = ({ id, name, price, desc, rating, img }) => {
   return (
-    <div>
+    <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2">
       <img
-        src="https://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5"
+        src={img}
         alt=""
+        className="w-auto h-[130px] hover:scale-110 cursor-grab transition-all duration-500 ease-in-out"
       />
-      <div>
-        <h2>Onion Pizza</h2>
-        <span>180</span>
+      <div className="text-sm flex justify-between">
+        <h2>{name}</h2>
+        <span className="text-green-500">₹{price}</span>
+      </div>
+      <p className="text-sm font-normal">{desc.slice(0, 50)}...</p>
+      <div className="flex justify-between">
+        <span className="flex justify-center items-center">
+          <AiFillStar className="text-yellow-400 mr-1" /> {rating}
+        </span>
+        <button className="p-1 bg-green-500 text-white hover:bg-green-600 rounded-lg text-sm">
+          Add to cart
+        </button>
       </div>
     </div>
   );
--- src/Components/FoodItems.jsx
+++ src/Components/FoodItems.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import FoodCard from "./FoodCard";
-
+import FoodData from "../Data/FoodData.js";
 const FoodItems = () => {
   return (
-    <div>
-      <FoodCard />
+    <div className="flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10">
+      {FoodData.map((food) => {
+        return (
+          <FoodCard
+            key={food.id}
+            id={food.id}
+            name={food.name}
+            price={food.price}
+            desc={food.desc}
+            rating={food.rating}
+            img={food.img}
+          />
+        );
+      })}
     </div>
   );
 };
