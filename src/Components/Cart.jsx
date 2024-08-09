ef2e1e35fd7d137fcc7984a1ecdbd0b34a1b4162
Anuj <invincibleanuj1718@gmail.com>
2024-08-09 18:55:51 +0530

    Updated Cart.jsx

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
+import ItemCard from "./ItemCard";
+
 const Cart = () => {
   return (
     <>
-      <div className="fixed right-0 top-0 w-full lg:w-[20vw] h-full bg-white p-5">
+      <div className="fixed right-0 top-0 w-full lg:w-[20vw] h-full bg-white p-5 mb-3">
         <div className="flex justify-between items-center my-3 ">
           <span className="text-xl font-bold text-gray-800">My Order</span>
           <IoMdClose className="border-2 border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-300 hover:border-red-300 cursor-pointer" />
         </div>
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+
         <div className="bottom-0 absolute">
           <h3 className="font-semibold text-gray-800">Items:</h3>
           <h3 className="font-semibold text-gray-800">Total Amount:</h3>
