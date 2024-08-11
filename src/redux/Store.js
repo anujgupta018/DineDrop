e87494a781d56df26a0567a68bd2ce7d85fb6986
Anuj <invincibleanuj1718@gmail.com>
2024-08-11 17:26:18 +0530

    Updated Store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./Slices/CartSlice";
+import CategorySlice from "./Slices/CategorySlice";
 const Store = configureStore({
   reducer: {
     cart: CartSlice,
+    category: CategorySlice,
   },
 });
 export default Store;
