9d5cb3e5cf05a17a72386c64db6f4cdc188dad5d
Anuj <invincibleanuj1718@gmail.com>
2024-08-12 21:18:06 +0530

    Updated checkout section

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -3,6 +3,8 @@ import { IoMdClose } from "react-icons/io";
 import ItemCard from "./ItemCard";
 import { useSelector } from "react-redux";
 import { FaShoppingCart } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
 const Cart = () => {
   const [activeCart, setActiveCart] = useState(false);
   const cartItems = useSelector((state) => state.cart.cart);
@@ -11,6 +13,7 @@ const Cart = () => {
     (total, item) => total + item.qty * item.price,
     0
   );
+  const navigate = useNavigate();
   return (
     <>
       <div
@@ -50,7 +53,10 @@ const Cart = () => {
             Total Amount:{totalPrice}
           </h3>
           <hr className="w-[90vw] lg:w-[18vw] my-2" />
-          <button className="bg-green-500 font-bold text-white px-3 py-2 rounded-lg w-[90vw] lg:w-[18vw] mb-5">
+          <button
+            onClick={() => navigate("/success")}
+            className="bg-green-500 font-bold text-white px-3 py-2 rounded-lg w-[90vw] lg:w-[18vw] mb-5"
+          >
             Checkout
           </button>
         </div>
--- src/Pages/Success.jsx
+++ src/Pages/Success.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { PropagateLoader } from "react-spinners";
 const Success = () => {
-  return <div>h</div>;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      {loading ? (
+        <PropagateLoader color="#29be24" />
+      ) : (
+        <div>
+          <h2 className="text-3xl font-semibold mb-4 text-center">
+            Order Successful!
+          </h2>
+          <p>Your Order has been successfully placed</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Success;
